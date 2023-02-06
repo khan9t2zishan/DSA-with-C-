f67770cc23b14fd430c316a012b13cdb917bc965
Zishan khan <106923884+khan9t2zishan@users.noreply.github.com>
2023-02-07 00:35:30 +0530

    finds the length of largest subarray with 0 sum

---
+++ 17subarray_sum_0.cpp
@@ -0,0 +1,33 @@
+//Program to find the length of largest subarray with 0 sum
+#include <bits/stdc++.h>
+using namespace std;
+int maxLen(int A[], int N)
+{
+	int max_len = 0;
+	for (int i = 0; i < N; i++) 
+    {
+		int curr_sum = 0;
+		for (int j = i; j < N; j++) 
+        {
+			curr_sum +=A[j];
+			if (curr_sum == 0)
+				max_len = max(max_len, j - i + 1);
+		}
+	}
+	return max_len;
+}
+int main()
+{
+	//int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+    int N;
+    cout<<"Enter the size of the array: ";
+    cin>>N;
+    int A[N];
+    cout<<"Enter the elements of the array:\n";
+    for(int i=0;i<N;i++)
+     cin>>A[i];
+	//int N = sizeof(arr) / sizeof(arr[0]);
+	cout << "Length of the longest 0 sum subarray is "
+		<< maxLen(A, N);
+	return 0;
+}
